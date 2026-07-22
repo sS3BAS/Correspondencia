204c66c6f5520545b2ba2b6fec73cf3c2eee26d6
sS3BAS <124070951+sS3BAS@users.noreply.github.com>
2026-07-22 17:13:34 -0600

    modelo correspondencia

--- app/Models/Correspondencia.php
+++ app/Models/Correspondencia.php
@@ -6,5 +6,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class Correspondencia extends Model
 {
-    //
+    protected $table = 'correspondencias';
+
+    protected $fillable = [
+        'tipo', 'numero_ficha', 'numero_control', 'fecha_registro',
+        'area_id', 'puesto_id', 'nombre_remitente', 'cargo_remitente',
+        'institucion', 'nombre_destinatario', 'cargo_destinatario',
+        'domicilio', 'tipo_documento', 'numero_fojas', 'anexos',
+        'asunto', 'prioridad', 'estado'
+    ];
+
+    public function area() {
+        return $this->belongsTo(Area::class);
+    }
+
+    public function puesto() {
+        return $this->belongsTo(Puesto::class);
+    }
+
+    public function entrega() {
+        return $this->hasOne(Entrega::class);
+    }
+
+    public function reparto() {
+        return $this->hasOne(Reparto::class);
+    }
+
+    public function acuse() {
+        return $this->hasOne(Acuse::class);
+    }
+
+    public function historial() {
+        return $this->hasMany(HistorialEstado::class);
+    }
 }
